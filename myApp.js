61a86e15fbd168bc460f60a7640f56a853b54cfe
elyahu631 <eanavi363@gmail.com>
2023-12-17 14:02:26 +0200

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 // myApp.js
+require("dotenv").config();
 let express = require("express");
 let app = express();
 const path = require("path");
 
-// Middleware for serving static files from 'public' directory
+// Middleware for serving static files
 app.use("/public", express.static(path.join(__dirname, "public")));
+
 // Route that handles GET requests to the root URL
 app.get("/", function (req, res) {
   const absolutePath = path.join(__dirname, "views", "index.html");
@@ -12,8 +14,12 @@ app.get("/", function (req, res) {
 });
 
 // Route for the /json path
-app.get('/json', function(req, res) {
-  res.json({"message": "Hello json"});
+app.get("/json", function (req, res) {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
 });
 
 console.log("Hello World"); //node myApp.js
