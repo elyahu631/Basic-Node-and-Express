b225456a51ad2178a375a16655b4c8b829d86277
elyahu631 <eanavi363@gmail.com>
2023-12-17 13:43:52 +0200

    Update the Route Handler:

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 // myApp.js
 let express = require("express");
 let app = express();
+const path = require('path'); 
 
 // Route that handles GET requests to the root URL
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  const absolutePath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(absolutePath);
 });
 
 console.log("Hello World"); //node myApp.js
