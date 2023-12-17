216eddc76ef38111d259ba0f8488799b0e47518f
elyahu631 <eanavi363@gmail.com>
2023-12-17 13:48:02 +0200

    serve static assets in an Express application

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 // myApp.js
 let express = require("express");
 let app = express();
-const path = require('path'); 
+const path = require("path");
 
+// Middleware for serving static files from 'public' directory
+app.use("/public", express.static(path.join(__dirname, "public")));
 // Route that handles GET requests to the root URL
-app.get('/', function(req, res) {
-  const absolutePath = path.join(__dirname, 'views', 'index.html');
+app.get("/", function (req, res) {
+  const absolutePath = path.join(__dirname, "views", "index.html");
   res.sendFile(absolutePath);
 });
 
